678911fc3dc9111ea9459005e9dd8b5f25e51367
aleafs <zhangxc83@gmail.com>
2012-04-21 22:18:24 +0800

    Develp update

---
+++ build/tpl/log_rotate_everyday.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: #
+
+declare -r CFG_LOG_PATH="##log.root##"
+declare -r _EXTENSION=$(date -- +"%Y%m%d")
+
--- build/tpl/log_rotate_sample.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/bash
-# vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: #
-
