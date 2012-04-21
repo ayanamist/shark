8e1bd630d199817b178f062997e403bec589e055
aleafs <zhangxc83@gmail.com>
2012-04-21 21:54:48 +0800

    Add ./build/jscoverage.sh

---
+++ build/jscoverage.sh
@@ -0,0 +1,24 @@
+# ! /bin/bash
+# vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: #
+
+declare -r __PATH=`pwd`
+declare -r __ROOT="$(cd -- $(dirname -- ${0}) && pwd)"
+declare -r JSCOVE="${__ROOT}/jscoverage"
+
+# {{{ function install_jscoverage() #
+function install_jscoverage() {
+if [ ! -d "${__ROOT}/tmp" ] ; then
+    mkdir -p "${__ROOT}/tmp"
+fi
+
+cd "${__ROOT}/tmp" && git clone git://github.com/visionmedia/node-jscoverage.git node-jscoverage && \
+    cd node-jscoverage && ./configure && make && \
+    cp ./jscoverage "${JSCOVE}" && chmod +x "${JSCOVE}" && rm -rf ${__ROOT}/tmp
+}
+# }}} #
+
+if [ ! -f "${JSCOVE}" ] ; then
+    install_jscoverage
+fi
+
+cd "${__PATH}"
