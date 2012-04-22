82e5d2b3738a2d91b72669f80a64a8f5da0a628b
aleafs <zhangxc83@gmail.com>
2012-04-22 18:22:33 +0800

    Add unittest for lib/cache.js

---
+++ test/unit/CacheTest.js
@@ -0,0 +1,71 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+// +--------------------------------------------------------------------+
+// | (C) 2011-2012 Alibaba Group Holding Limited.                       |
+// | This program is free software; you can redistribute it and/or      |
+// | modify it under the terms of the GNU General Public License        |
+// | version 2 as published by the Free Software Foundation.            |
+// +--------------------------------------------------------------------+
+// Author: pengchun <pengchun@taobao.com>
+
+var should  = require('should');
+var Cache   = require(__dirname + '/../../lib/cache.js');
+
+/* {{{ private function Handle() */
+/**
+ * @缓存存储引擎
+ */
+var Handle  = function() {
+
+  var _obj  = {};
+
+  var _me   = {};
+
+  _me.set   = function(key, value, callback) {
+    _obj[key]   = value;
+    callback(null);
+  };
+
+  _me.get   = function(key, callback) {
+    callback(null, _obj[key]);
+  };
+
+  _me.delete    = function(key, callback) {
+    delete _obj[key];
+    callback(null);
+  };
+
+  return _me;
+};
+/* }}} */
+
+describe('cache protocol', function() {
+
+  /* {{{ should_cache_set_and_get_works_fine() */
+  it('should_cache_set_and_get_works_fine', function(done) {
+    var num = 2;
+    var res = Handle();
+    var _me = Cache.create('test1', res);
+
+    _me.set('key1', {'a' : 'val1', 'b' : [2]}, function(error) {
+      should.ok(!error);
+      _me.get('key1', function(error, value, expire) {
+        should.ok(!error);
+        value.should.eql({'a' : 'val1', 'b' : [2]});
+        expire.should.eql(2);
+        if ((--num) <= 0) {
+          done();
+        }
+      });
+    }, 2);
+
+    _me.get('i am not exists', function(error, value, expire) {
+      should.ok(!error);
+      should.ok(null === value);
+      if ((--num) <= 0) {
+        done();
+      }
+    });
+  });
+  /* }}} */
+
+});
