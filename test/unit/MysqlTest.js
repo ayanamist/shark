8753f49a6e3e463e440eafced264bb9bbff50b18
aleafs <zhangxc83@gmail.com>
2012-04-24 22:26:16 +0800

    set autocommit = 1 when connected

--- test/unit/MysqlTest.js
+++ test/unit/MysqlTest.js
@@ -68,20 +68,21 @@ describe('mysql pool with libmysqlclient', function() {
   /* {{{ should_mysql_query_works_fine() */
   it('should_mysql_query_works_fine', function(done) {
     var _me = Mysql.create(options);
+    var sql = 'CREATE TABLE test.only_for_unittest (' + 
+      'id int(10) unsigned not null auto_increment primary key,'+
+      'txt varchar(2) not null default \'\'' +
+      ')ENGINE=MYISAM';
+
     _me.query('DROP TABLE IF EXISTS test.only_for_unittest', function(error, info) {
       should.ok(!error);
       info.should.have.property('affectedRows', 0);
 
-      _me.query('CREATE TABLE test.only_for_unittest (' + 
-          'id int(10) unsigned not null auto_increment primary key,'+
-          'txt varchar(2) not null default \'\'' +
-          ')ENGINE=MYISAM', function(error, info) {
+      _me.query(sql, function(error, info) {
         should.ok(!error);
         _me.query('INSERT INTO test.only_for_unittest (txt) VALUES (\'test\')', function(error, info) {
           info.should.have.property('insertId', 1);
           info.should.have.property('affectedRows', 1);
           _me.query('SELECT * FROM test.only_for_unittest', function(error, rows) {
-            console.log(error);
             rows.should.eql([{
               'id'  : 1,
               'txt' : 'te',
@@ -91,7 +92,7 @@ describe('mysql pool with libmysqlclient', function() {
         });
           });
         });
-  });
-  /**/
+      });
+    /**/
 
 });
