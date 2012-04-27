96f2877ad79c2e06b1ab77a94d328302cb97cc10
aleafs <zhangxc83@gmail.com>
2012-04-27 20:32:33 +0800

    Add libevent install for memcached

--- build/memcache.sh
+++ build/memcache.sh
@@ -6,12 +6,21 @@ declare -r __ROOT="$(cd -- $(dirname -- ${0}) && pwd)"
 declare -r MEMCACHE="${__ROOT}/memcached"
 
 install_memcached() {
-    if [ ! -d "${__ROOT}/tmp" ] ; then
-        mkdir -p "${__ROOT}/tmp"
+    if [ -d "${__ROOT}/tmp" ] ; then
+        /bin/rm -rf "${__ROOT}/tmp"
     fi
-    cd "${__ROOT}/tmp" && wget "http://memcached.googlecode.com/files/memcached-1.4.13.tar.gz" && \
-        tar zxvf ./memcached-1.4.13.tar.gz && cd memcached-1.4.13 && ./configure && make && \
-        cp ./memcached "${MEMCACHE}" && chmod +x "${MEMCACHE}" && /bin/rm -rf ${__ROOT}/tmp
+
+    mkdir -p "${__ROOT}/tmp"
+    cd "${__ROOT}/tmp" && \
+        wget --no-check-certificate "https://github.com/downloads/libevent/libevent/libevent-2.0.18-stable.tar.gz" && \
+        tar zxvf ./libevent-2.0.18-stable.tar.gz && cd libevent-2.0.18-stable && \
+        ./configure && make && \
+        cd "${__ROOT}/tmp" && \
+        wget "http://memcached.googlecode.com/files/memcached-1.4.13.tar.gz" && \
+        tar zxvf ./memcached-1.4.13.tar.gz && cd memcached-1.4.13 && \
+        ./configure --with-libevent=${__ROOT}/tmp/libevent-2.0.18-stable && make && \
+        cp ./memcached "${MEMCACHE}" && chmod +x "${MEMCACHE}" && \
+        /bin/rm -rf ${__ROOT}/tmp
 }
 
 if [ ! -f "${MEMCACHE}" ] ; then
