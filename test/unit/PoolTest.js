cfd1ba503307d77e31e25367d48dadb41ce99717
aleafs <zhangxc83@gmail.com>
2012-05-03 21:10:55 +0800

    generic pool

---
+++ test/unit/PoolTest.js
@@ -0,0 +1,21 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+// +--------------------------------------------------------------------+
+// | (C) 2011-2012 Alibaba Group Holding Limited.                       |
+// | This program is free software; you can redistribute it and/or      |
+// | modify it under the terms of the GNU General Public License        |
+// | version 2 as published by the Free Software Foundation.            |
+// +--------------------------------------------------------------------+
+// Author: pengchun <pengchun@taobao.com>
+
+var should  = require('should');
+var Pool    = require(__dirname + '/../../lib/pool.js');
+
+describe('connection pool', function() {
+
+  /* {{{ should_pool_create_works_fine() */
+  it('should_pool_create_works_fine', function(done) {
+    done();
+  });
+  /* }}} */
+
+});
