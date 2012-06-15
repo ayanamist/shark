50d96bd20edd186963248c8e211f274c07d6afeb
aleafs <zhangxc83@gmail.com>
2012-06-15 18:04:50 +0800

    lib/error.js unit test

--- test/unit/ErrorTest.js
+++ test/unit/ErrorTest.js
@@ -6,8 +6,23 @@ describe('error interface', function () {
 
   var iError  = require(__dirname + '/../../lib/error.js');
 
-  it('should', function () {
+  /* {{{ should_error_have_name_property() */
+  it('should_error_have_name_property', function () {
     var _me = new iError('Hello ', 'i am only test error');
     _me.should.have.property('name', 'Hello');
+    _me.should.have.property('message', 'i am only test error');
   });
+  /* }}} */
+
+  /* {{{ should_get_error_stack_works_fine() */
+  it('should_get_error_stack_works_fine', function () {
+    try {
+        throw new iError(' Hloo', 'error message');
+    } catch (e) {
+      e.stack.should.include('Hloo: error message');
+      e.stack.should.include('at new <anonymous> (');
+    }
+  });
+  /* }}} */
+
 });
