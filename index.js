373b5c3b0aab9f97e8665ed5dac91ef4db64ebe3
aleafs <zhangxc83@gmail.com>
2012-07-26 13:47:21 +0800

    alallflelf

--- index.js
+++ index.js
@@ -2,13 +2,33 @@
 
 "use strict";
 
-var shark = {};
+var Shark = {};
 
 require('fs').readdirSync(__dirname + '/lib').forEach(function (item) {
   var m = item.match(/(.+?)\.js$/);
   if (m && m[1]) {
-    shark[m[1]] = require(__dirname + '/lib/' + m[0]);
+    Shark[m[1]] = require(__dirname + '/lib/' + m[0]);
   }
 });
 
-module.exports = shark;
+Shark.run = function (options) {
+
+  /**<    node_modules/shark/index.js */
+  var _root = __dirname + '/../..';
+
+  /**
+   * @ 配置项
+   */
+  var _conf = {
+    'home'      : _root,
+    'logpath'   : _root + '/log',
+    'runpath'   : _root + '/run',
+    'etcpath'   : _root + '/etc',
+  };
+  for (var i in options) {
+    _conf[i] = options[i];
+  }
+
+};
+
+module.exports = Shark;
