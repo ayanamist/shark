c73b44179e7d6cf13e87133fed3331e98628a7cf
aleafs <zhangxc83@gmail.com>
2012-07-26 17:47:10 +0800

    Add shark compile

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ require('fs').readdirSync(__dirname + '/lib').forEach(function (item) {
   }
 });
 
-Shark.run = function (options) {
+Shark.compile = function (options) {
 
+  /**
+   * @ 项目根路径
+   */
   /**<    node_modules/shark/index.js */
   var _root = __dirname + '/../..';
 
@@ -21,14 +24,37 @@ Shark.run = function (options) {
    */
   var _conf = {
     'home'      : _root,
+    'propfile'  : _root + '/default.properties',
     'logpath'   : _root + '/log',
     'runpath'   : _root + '/run',
     'etcpath'   : _root + '/etc',
+    'tplpath'   : _root + '/build/tpl',
   };
   for (var i in options) {
     _conf[i] = options[i];
   }
 
+  var path  = require('path');
+  if (!path.existsSync(_conf.propfile)) {
+    throw new Error('Property file "' + _conf.propfile + '" not found.');
+  }
+
+  var build = Shark.build;
+  var maker = build.init(_conf.propfile, _conf.home);
+  [_conf.logpath, _conf.runpath, _conf.etcpath].forEach(function (item) {
+    maker.makedir(item);
+  });
+
+  build.fileset(_conf.tplpath, function (fname) {
+    var _base = fname.slice(1 + _conf.tplpath.length);
+    if (_base.match(/\.properties$/)) {
+      return;
+    }
+
+    var _file = _conf.etcpath + '/' + _base;
+    maker.makedir(path.dirname(_file));
+    maker.makeconf(fname, _file);
+  });
 };
 
 module.exports = Shark;
