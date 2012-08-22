ce7969b974cf44d75bea559f46171d6afacbceba
aleafs <zhangxc83@gmail.com>
2012-08-22 11:53:44 +0800

    Add test case for empty result

--- test/unit/MysqlTest.js
+++ test/unit/MysqlTest.js
@@ -9,11 +9,11 @@ var Mysql   = require(__dirname + '/../../lib/mysql.js');
  */
 var options = config.create(__dirname + '/etc/mysql_test.ini').all();
 
-describe('mysql pool with libmysqlclient', function() {
+describe('mysql pool with libmysqlclient', function () {
 
   /* {{{ should_mysql_with_4_conn_pool_works_fine() */
-  it('should_mysql_with_4_conn_pool_works_fine', function(done) {
-    Mysql.create(options).query('SELECT 1', function(error, rows, info) {
+  it('should_mysql_with_4_conn_pool_works_fine', function (done) {
+    Mysql.create(options).query('SELECT 1', function (error, rows, info) {
       should.ok(!error);
       rows.should.eql([{'1':'1'}]);
       done();
@@ -22,16 +22,16 @@ describe('mysql pool with libmysqlclient', function() {
   /* }}} */
 
   /* {{{ should_sleep_100ms_async_run_works_fine() */
-  it('should_sleep_100ms_async_run_works_fine', function(done) {
+  it('should_sleep_100ms_async_run_works_fine', function (done) {
     var mysql   = Mysql.create(options);
     var total   = 5;
     var dones   = 0;
     var times   = [];
     for (var i = 0; i < total; i++) {
       times[i]  = (new Date()).getTime();
-      (function() {
+      (function () {
         var c   = i;
-        mysql.query('SELECT ' + c + ' AS k,SLEEP(0.1)', function(error, rows, info) {
+        mysql.query('SELECT ' + c + ' AS k,SLEEP(0.1)', function (error, rows, info) {
           should.ok(!error);
           rows.should.eql([{
             'k'   : c + '',
@@ -49,9 +49,9 @@ describe('mysql pool with libmysqlclient', function() {
   /* }}} */
 
   /* {{{ should_mysql_blackhole_works_fine() */
-  it('should_mysql_blackhole_works_fine', function(done) {
+  it('should_mysql_blackhole_works_fine', function (done) {
     var mysql   = require(__dirname + '/../../lib/blackhole/mysql.js').create();
-    mysql.query('select', function(error, data) {
+    mysql.query('select', function (error, data) {
       error.toString().should.include('MysqlBlackhole');
       done();
     });
@@ -59,7 +59,7 @@ describe('mysql pool with libmysqlclient', function() {
   /* }}} */
 
   /* {{{ should_mysql_query_works_fine() */
-  it('should_mysql_query_works_fine', function(done) {
+  it('should_mysql_query_works_fine', function (done) {
     options.dbname  = 'test';
     var _me = Mysql.create(options);
     var sql = 'CREATE TABLE only_for_unittest (' + 
@@ -67,21 +67,25 @@ describe('mysql pool with libmysqlclient', function() {
       'txt varchar(2) not null default ""' +
       ')ENGINE=MYISAM';
 
-    _me.query('DROP TABLE IF EXISTS only_for_unittest', function(error, info) {
+    _me.query('DROP TABLE IF EXISTS only_for_unittest', function (error, info) {
       should.ok(!error);
       info.should.have.property('affectedRows', 0);
 
-      _me.query(sql, function(error, info) {
+      _me.query(sql, function (error, info) {
         should.ok(!error);
-        _me.query('INSERT INTO only_for_unittest (txt) VALUES ("test")', function(error, info) {
+        _me.query('INSERT INTO only_for_unittest (txt) VALUES ("test")', function (error, info) {
           info.should.have.property('insertId', 1);
           info.should.have.property('affectedRows', 1);
-          _me.query('SELECT id,txt FROM test.only_for_unittest', function(error, rows) {
+          _me.query('SELECT id,txt FROM test.only_for_unittest', function (error, rows) {
             rows.should.eql([{
               'id'  : 1,
               'txt' : 'te',
             }]);
-            done();
+            _me.query('SELECT * FROM test.only_for_unittest WHERE 0', function (error, rows) {
+              should.ok(!error);
+              rows.should.have.property('length', 0);
+              done();
+            });
           });
         });
       });
